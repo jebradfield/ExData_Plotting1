3a2f188a3a169d90316411876eb7f9e93a15aa1f
jasonbradfield <jasonbradfield@gmail.com>
2014-05-07 19:03:46 -0700

    third commit

--- plot1.R
+++ plot1.R
@@ -1,7 +1,7 @@
 pow = read.table('household_power_consumption.txt', header=TRUE, sep=';')
 pow$DateTime=paste(as.character(pow$Date),as.character(pow$Time))
 pow$DateTime=strptime(pow$DateTime, "%d/%m/%Y %H:%M:%s")
-
+#x
 pows=subset(pow,pow$DateTime>=as.POSIXlt("2007-02-01 00:00:00") & pow$DateTime<as.POSIXlt("2007-02-03 00:00:00"))
 pows$Global_active_power2=as.numeric(as.character(pows$Global_active_power))
 pows$Sub_metering_12=as.numeric(as.character(pows$Sub_metering_1))
--- plot3.R
+++ plot3.R
@@ -13,7 +13,7 @@ pows$Global_reactive_power2=as.numeric(as.character(pows$Global_reactive_power))
 plot(pows$DateTime,pows$Sub_metering_12, type='l',xlab='',ylab='Energy sub metering', cex.lab=.8, cex.axis=.8)
 lines(pows$DateTime,pows$Sub_metering_22, type='l', col='red')
 lines(pows$DateTime,pows$Sub_metering_3, type='l', col='blue')
-legend('topright', legend=c('Sub_metering_1','Sub_metering_2','Sub_metering_3'), col=c('black','red','blue'),lty=1,cex=.78)
+legend('topright', legend=c('Sub_metering_1 ','Sub_metering_2 ','Sub_metering_3 '), col=c('black','red','blue'),lty=1,cex=.70)
 
 dev.copy(png, file = 'plot3.png', width = 480, height = 480)
 
